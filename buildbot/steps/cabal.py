544f1f35e435de34f1f163f1f5e18d48b1586913
Roman Cheplyaka <roma@ro-che.info>
2014-02-17 15:25:06 +0200

    Initial

---
+++ buildbot/steps/cabal.py
@@ -0,0 +1,36 @@
+from buildbot.steps.shell import ShellCommand
+from itertools import chain
+
+class Cabal:
+    sandbox = None
+    optimization = 0
+    jobs = 1
+
+    def sandboxOpt(self):
+        if not self.sandbox is None:
+            yield "--sandbox-config-file={0}/cabal.sandbox.config".format(self.sandbox)
+
+    def optimizationOpt(self):
+        yield "--ghc-option=-O{0}".format(self.optimization)
+
+    def jobsOpt(self):
+        yield "-j{0}".format(self.jobs)
+
+    def allOpts(self):
+        chain(self.sandboxOpt, self.optimizationOpt, self.jobsOpt)
+
+    def update(self, **kwargs):
+        return ShellCommand(
+            name="cabal update",
+            description="Downloading the latest package list",
+            command=list(chain(["cabal", "update"], self.allOpts)),
+            **kwargs
+        )
+
+    def install(self, package, **kwarg):
+        return ShellCommand(
+            name="cabal install",
+            description="Installing package {0}".format(package),
+            command=list(chain(["cabal", "install", package], self.allOpts)),
+            **kwargs
+        )
