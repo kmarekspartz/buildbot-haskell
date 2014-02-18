05bd54a7916ddc4d5de6499e25080c10181e045b
Roman Cheplyaka <roma@ro-che.info>
2014-02-18 15:06:00 +0200

    Use 'config'

--- buildbot_haskell/cabal.py
+++ buildbot_haskell/cabal.py
@@ -2,6 +2,10 @@ from buildbot.steps.shell import ShellCommand
 from itertools import chain
 import pipes
 
+def union(x,y):
+    """Right-biased dictionary union"""
+    return dict(x.items() + y.items())
+
 class Cabal:
     """
     An object of the :py:class:`Cabal` class is used to issue ``cabal`` commands
@@ -19,48 +23,49 @@ class Cabal:
     :py:obj:`jobs` specifies an optional sandbox directory. If it is not
     :py:const:`None`, all cabal commands will be sandboxed.
     """
-    def __init__ (self, sandbox = None, optimization = 0, jobs = 1):
-        self.sandbox = sandbox
-        self.optimization = optimization
-        self.jobs = jobs
+    default_config = { 'sandbox': None, 'optimization': 0, 'jobs': 1 }
 
-    def __sandboxOpt(self):
-        if not self.sandbox is None:
-            yield "--sandbox-config-file={0}/cabal.sandbox.config".format(self.sandbox)
+    def __init__ (self, **config):
+        self.config = union(default_config, config)
 
-    def __optimizationOpt(self):
-        yield "--ghc-option=-O{0}".format(self.optimization)
+    def __sandboxOpt(self, config):
+        sandbox = union(self.config, config)['sandbox']
+        if not sandbox is None:
+            yield "--sandbox-config-file={0}/cabal.sandbox.config".format(sandbox)
+
+    def __optimizationOpt(self, config):
+        yield "--ghc-option=-O{0}".format(union(self.config, config)['optimization'])
 
     def __jobsOpt(self):
-        yield "-j{0}".format(self.jobs)
+        yield "-j{0}".format(union(self.config, config)['jobs'])
 
-    def __allOpts(self):
-        return chain(self.__sandboxOpt(), self.__optimizationOpt(), self.__jobsOpt())
+    def __allOpts(self, config):
+        return chain(self.__sandboxOpt(config), self.__optimizationOpt(config), self.__jobsOpt(config))
 
-    def update(self, **kwargs):
+    def update(self, **config):
         """Run ``cabal update``"""
         return ShellCommand(
             name="cabal update",
             description="Downloading the latest package list",
             command=["cabal", "update"],
-            **kwargs
+            **config
         )
 
-    def install(self, package, **kwargs):
+    def install(self, package, **config):
         """Run ``cabal install package``"""
         return ShellCommand(
             name="cabal install {0}".format(package),
-            command=list(chain(["cabal", "install", package], self.__allOpts())),
-            **kwargs
+            command=list(chain(["cabal", "install", package], self.__allOpts(config))),
+            **config
         )
 
-    def __sandbox_check(self):
-        if self.sandbox is None:
-            raise ValueError(
-                "sandbox_init: sandbox is not defined.\n"
-                "Provide the sandbox argument when creating a Cabal object")
+    def __get_sandbox(self, config):
+        sandbox = union(self.config, config)['sandbox']
+        if sandbox is None:
+            raise ValueError("sandbox is not defined")
+        return sandbox
 
-    def sandbox_init(self, **kwargs):
+    def sandbox_init(self, **config):
         """
         Run ``cabal sandbox init`` in the sandbox directory specified during the
         :py:class:`Cabal` instance creation
@@ -70,23 +75,23 @@ class Cabal:
         .. note::
           ``sandbox_init`` probably won't work with a Windows slave. Patches are welcome.
         """
-        self.__sandbox_check()
+        sandbox = self.__get_sandbox(config)
         return ShellCommand(
             name="cabal sandbox init",
-            description="Initializing sandbox at {0}".format(self.sandbox),
-            command="mkdir -p {0} && cd {0} && cabal sandbox init".format(pipes.quote(self.sandbox))
+            description="Initializing sandbox at {0}".format(sandbox),
+            command="mkdir -p {0} && cd {0} && cabal sandbox init".format(pipes.quote(sandbox))
         )
-    def sandbox_delete(self, **kwargs):
+    def sandbox_delete(self, **config):
         """
         Run ``cabal sandbox delete`` in the sandbox directory specified during the
         :py:class:`Cabal` instance creation.
 
         The directory itself is not removed.
         """
-        self.__sandbox_check()
+        sandbox = self.__get_sandbox(config)
         return ShellCommand(
             name="cabal sandbox delete",
-            description="Deleting sandbox at {0}".format(self.sandbox),
-            workdir=self.sandbox,
+            description="Deleting sandbox at {0}".format(sandbox),
+            workdir=sandbox,
             command=["cabal","sandbox","delete"]
         )
